98fe93fa89efa82045dda41dd7fed8495f1b349d
Keanu Ellary <u22893459@tuks.co.za>
2025-05-20 21:53:00 +0200

    Adding folder structure

---
+++ backend/main.go
@@ -0,0 +1,12 @@
+package main
+
+// "log"
+
+func main() {
+	// r := routers.SetupRouter()
+
+	// log.Println("Starting backend server on: http://localhost:8080")
+	// if error := r.Run(":8080"); error != nil {
+	// 	log.Fatalf("Error starting server: %v", error)
+	// }
+}
