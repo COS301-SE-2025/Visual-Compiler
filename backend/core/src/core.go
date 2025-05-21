1714c2e35c166a37c0ff3f2a2aa328a2c74a4427
Tia-H <tiaharripersad@gmail.com>
2025-05-21 21:27:57 +0200

    file structure for core backend

---
+++ backend/core/src/core.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/COS301-SE-2025/Visual-Compiler/backend/api/routers"
+	"github.com/COS301-SE-2025/Visual-Compiler/backend/core/db"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func startServer()
+[
+
+]
