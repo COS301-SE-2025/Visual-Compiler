f2691c18e9932a1da581cbfb542e533afd0170b9
sashen-tv <sashengajai.sg@gmail.com>
2025-05-26 03:16:09 +0200

    Read types and regex rules

--- visual-compiler/backend/core/services/core.go
+++ visual-compiler/backend/core/services/core.go
@@ -26,3 +26,30 @@ var tokens []TypeValue
 func Initialise(data string) {
 	source_code = data
 }
+
+func ReadTypeRegex(input []byte) {
+
+	rules = []TypeRegex{}
+
+	var raw_pairs []struct {
+		Type  string `json:"type"`
+		Regex string `json:"regex"`
+	}
+
+	err := json.Unmarshal(input, &raw_pairs)
+
+	if err != nil {
+		fmt.Printf("invalid input: %s", err)
+	}
+
+	for _, pair := range raw_pairs {
+
+		_, err := regexp.Compile(pair.Regex)
+
+		if err != nil {
+			fmt.Printf("invalid regex: %s", err)
+		}
+
+		rules = append(rules, TypeRegex{Type: pair.Type, Regex: pair.Regex})
+	}
+}
