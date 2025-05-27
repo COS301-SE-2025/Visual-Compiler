907f42c0bbb9a5fd35cbb48f16184b80bb66616b
sashen-tv <sashengajai.sg@gmail.com>
2025-05-27 22:45:21 +0200

    Fixed edge cases for tokenisation

--- visual-compiler/backend/core/services/core.go
+++ visual-compiler/backend/core/services/core.go
@@ -1,5 +1,4 @@
-//Package path implements routines for the phases of compiler construction
-//This packages handles the lexing process for compiler construction
+// Routines for compiler construction phases
 package services
 
 import (
@@ -9,55 +8,50 @@ import (
 	"strings"
 )
 
-//String variable that stores the source code
+// String variable that stores the source code
 var source string
 
-//Struct that indicates how the RegexRules
+// Struct for the type and regex pairs
 type TypeRegex struct {
 	Type  string
 	Regex string
 }
 
-//Rules array variable of type TypeRegex
-//Stores each regex rule 
+// Array that stores the user's regex rules
 var rules []TypeRegex
 
-//Struct that stores the Type and Value of a token
+// Struct for the tokens
 type TypeValue struct {
 	Type  string
 	Value string
 }
 
-//Tokens array to store each Type and the Value for each token
+// Array that stores the user's generated tokens
 var tokens []TypeValue
 
-//Unexpected tokens array to store each Type and the Value for each token that was not identified
-var unexpected_tokens []string
+// Array that stores unidentified input
+var tokens_unidentified []string
 
-//Name: SourceCode
-//Paramters: <string> which will be source code
-//Return: none
-//Setter function which sets the source variable to the value of parameter
+// Name: SourceCode
+// Parameters: <string> which will be source code
+// Return: none
+// Setter function which sets the source variable to the value of parameter
 func SourceCode(data string) {
-
 	source = data
-
 }
 
-//Name: GetSourceCode
-//Parameters: none
-//Return: <string>
-//Getter function to get the value of the source variable
+// Name: GetSourceCode
+// Parameters: none
+// Return: <string>
+// Getter function to get the value of the source variable
 func GetSourceCode() string {
-
 	return source
-
 }
 
-//Name: ReadRegexRules
-//Parameters: []byte
-//Return: error
-//This function will receive an array of regex rules, validate them and store them in the rules struct
+// Name: ReadRegexRules
+// Parameters: []byte
+// Return: error
+// Receive an array of regex rules, validate them and store them in the rules struct
 func ReadRegexRules(input []byte) error {
 
 	rules = []TypeRegex{}
@@ -70,7 +64,7 @@ func ReadRegexRules(input []byte) error {
 	err := json.Unmarshal(input, &raw_pairs)
 
 	if err != nil {
-		return fmt.Errorf("invalid JSON input: %w", err)
+		return fmt.Errorf("tokens_unidentified JSON input: %w", err)
 	}
 
 	for _, pair := range raw_pairs {
@@ -78,7 +72,7 @@ func ReadRegexRules(input []byte) error {
 		_, err := regexp.Compile(pair.Regex)
 
 		if err != nil {
-			return fmt.Errorf("invalid regex: %w", err)
+			return fmt.Errorf("tokens_unidentified regex: %w", err)
 		}
 
 		rules = append(rules, TypeRegex{Type: strings.ToUpper(pair.Type), Regex: pair.Regex})
@@ -87,47 +81,60 @@ func ReadRegexRules(input []byte) error {
 	return nil
 }
 
-//Name: CreateTokens
-//Parameters: none
-//Return: <[]TypeValue> An array of tokens
-//This function will loop through the source code to find all tokens that match the regex rules stored,
-//each match will be stored in the tokens array
-//Unexpected tokens will also be 
-func CreateTokens() []TypeValue {
+// Name: CreateTokens
+// Parameters: none
+// Return: none
+// Loop through the source code to find all tokens that match the regex rules stored
+func CreateTokens() {
 
-	unexpected_tokens = []string{}
 	tokens = []TypeValue{}
-	var words = strings.Fields(source)
+	tokens_unidentified = []string{}
 
-	for _, word := range words {
+	re := regexp.MustCompile(`\s+`)
+	input := re.ReplaceAllString(source, "")
+
+	length := len(input)
+	i := 0
+
+	for i < length {
 
 		found := false
 
 		for _, rule := range rules {
 
-			re := regexp.MustCompile("^" + rule.Regex + "$")
+			re := regexp.MustCompile("^" + rule.Regex)
+			position := re.FindStringIndex(input[i:])
 
-			if re.MatchString(word) {
+			if position != nil && position[0] == 0 {
+
+				match := input[i : i+position[1]]
+				tokens = append(tokens, TypeValue{Type: rule.Type, Value: match})
+				i = i + position[1]
 				found = true
-				tokens = append(tokens, TypeValue{Type: rule.Type, Value: word})
 				break
 			}
 		}
 
 		if !found {
-			fmt.Printf("unexpected token type: %q\n", word)
-			unexpected_tokens = append(unexpected_tokens,word)
+
+			tokens_unidentified = append(tokens_unidentified, string(input[i]))
+			i++
 		}
 	}
+}
 
+// Name: GetTokens
+// Parameters: none
+// Return: <[]TypeValue>
+// Getter function that returns the array of tokens
+func GetTokens() []TypeValue {
 	return tokens
 }
 
-//Name: GetUnexpectedTokens
-//Parameters: none
-//Return: <[]string> An array of string values
-//Getter function
-//This function will return the array of unexpected tokens found
-func GetUnexpectedTokens() []string {
-	return unexpected_tokens
+// Name: GetInvalidInput
+// Parameters: none
+// Return: <[]string>
+// Getter function that returns the array of unidentified input
+func GetInvalidInput() []string {
+	return tokens_unidentified
 }
