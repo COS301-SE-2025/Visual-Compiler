5881fb6eaeffe1b4a489280c43ec5a536969a622
Tia-H <tiaharripersad@gmail.com>
2025-05-28 05:34:13 +0200

    testing/unit-testing for frontend functions

--- visual-compiler/frontend/tests/unit-tests/main.test.ts
+++ visual-compiler/frontend/tests/unit-tests/main.test.ts
@@ -1,33 +1,7 @@
 import {describe,expect,vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
-import { fireEvent, render, screen,waitFor } from '@testing-library/svelte';
+import { fireEvent, render, screen} from '@testing-library/svelte';
 import page_comp from '../../src/routes/main/+page.svelte';
-import toasts from '../../src/lib/components/ToastContainer.svelte';
-import { theme } from '$lib/stores/theme';
-import { addToast } from '$lib/stores/toast';
-
-// Mock stores and dependencies
-vi.mock('$lib/stores/theme', () => ({
-  theme: {
-    subscribe: vi.fn((fn) => {
-      fn('light');
-      return { unsubscribe: vi.fn() };
-    })
-  }
-}));
-
-vi.mock('$lib/stores/toast', () => ({
-  addToast: vi.fn()
-}));
-
-const mockSuccessResponse = {
-    ok:true,
-    json: async()=> ({message:"Success"})
-};
-const mockFailedResponse = {
-    ok: false,
-    json: async () => ({error: "Failed"})
-};
 
 describe("Testing main page", () => {
     it("Page rendering", () => {
@@ -42,20 +16,33 @@ describe("Testing main page", () => {
         expect(screen.getByRole('button', {name: 'Lexer Converts source code into tokens for processing.'})).toBeInTheDocument();
     })
 
-    /*it("Test CreateNode-SourceCode",async() => {
+    it("Test CreateNode-SourceCode",async() => {
         render(page_comp);
     
         const button = screen.getByRole('button', {name: 'Source Code Start here. Add source code to begin compilation.'});
         await fireEvent.click(button);
-       expect(screen.getByRole('button', { name: /Source Code Node/i })).toBeInTheDocument();
+        expect(document.getElementById('N-source-1')).toBeInTheDocument();
 
     })
+
     it("Test CreateNode-Lexer",async() => {
         render(page_comp);
     
         const button = screen.getByRole('button', {name: 'Lexer Converts source code into tokens for processing.'});
         await fireEvent.click(button);
-       expect(screen.getByRole('button', { name: /Source Code Node/i })).toBeInTheDocument();
+        expect(document.getElementById('N-lexer-1')).toBeInTheDocument();
 
-    })*/
+    })
+
+    it("Test Create Multiple Nodes", async() => {
+        render(page_comp);
+    
+        const button = screen.getByRole('button', {name: 'Source Code Start here. Add source code to begin compilation.'});
+        await fireEvent.click(button);
+        expect(document.getElementById('N-source-1')).toBeInTheDocument();
+
+        const lbutton = screen.getByRole('button', {name: 'Lexer Converts source code into tokens for processing.'});
+        await fireEvent.click(lbutton);
+        expect(document.getElementById('N-lexer-2')).toBeInTheDocument();
+    })
 });
