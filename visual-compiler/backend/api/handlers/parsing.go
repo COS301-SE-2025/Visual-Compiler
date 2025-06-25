b5d14f04c37faaa09f44294138b0caa450ef38c9
Keanu-Ellary <161315536+Keanu-Ellary@users.noreply.github.com>
2025-06-25 20:28:25 +0200

    #161/feature/created files for parsing endpoints

---
+++ visual-compiler/backend/api/handlers/parsing.go
@@ -0,0 +1,14 @@
+package handlers
+
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"net/http"
+// 	"time"
+
+// 	"github.com/COS301-SE-2025/Visual-Compiler/backend/core/db"
+// 	"github.com/COS301-SE-2025/Visual-Compiler/backend/core/services"
+// 	"github.com/gin-gonic/gin"
+// 	"go.mongodb.org/mongo-driver/v2/bson"
+// 	"go.mongodb.org/mongo-driver/v2/mongo"
+// )
---
+++ visual-compiler/backend/api/routers/parsing-router.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	// "github.com/COS301-SE-2025/Visual-Compiler/backend/api/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+// Sets up the router for the parsing specific endpoints
+// Sets up the following:
+//   - POST request: parsing functionality
+func SetupParsingRouter() *gin.Engine {
+	r := gin.New()
+
+	return r
+}
