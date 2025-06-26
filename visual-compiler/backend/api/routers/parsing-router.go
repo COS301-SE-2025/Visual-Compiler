b69de50bce1f40fff8f30b9958571d426efe67b2
Keanu-Ellary <161315536+Keanu-Ellary@users.noreply.github.com>
2025-06-26 11:27:40 +0200

    #161/feature/endpoints for parsing completed

--- visual-compiler/backend/api/routers/parsing-router.go
+++ visual-compiler/backend/api/routers/parsing-router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	// "github.com/COS301-SE-2025/Visual-Compiler/backend/api/handlers"
+	"github.com/COS301-SE-2025/Visual-Compiler/backend/api/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,9 @@ import (
 //   - POST request: parsing functionality
 func SetupParsingRouter() *gin.Engine {
 	r := gin.New()
+
+	r.POST("/grammar", handlers.ReadGrammar)
+	r.POST("/tree", handlers.CreateSyntaxTree)
 
 	return r
 }
