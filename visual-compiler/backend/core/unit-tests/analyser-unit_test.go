cac549b820f80ac1e908939be67bd72e382ed0f5
Tia-H <tiaharripersad@gmail.com>
2025-07-14 05:28:10 +0200

    testing/symbol table code testing added

--- visual-compiler/backend/core/unit-tests/analyser-unit_test.go
+++ visual-compiler/backend/core/unit-tests/analyser-unit_test.go
@@ -98,3 +98,172 @@ func TestCreateSymbolTable_Valid(t *testing.T) {
 	}
 
 }
+
+func TestCreateSymbolTable_SameSymbolNames(t *testing.T) {
+
+	scope_rules := []*services.ScopeRule{
+		{Start: "{", End: "}"},
+	}
+
+	syntax_tree := services.SyntaxTree{
+		Root: &services.TreeNode{
+			Symbol: "MAIN",
+			Value:  "",
+			Children: []*services.TreeNode{
+				{
+					Symbol: "STATEMENT",
+					Value:  "",
+					Children: []*services.TreeNode{
+						{
+							Symbol: "DECLARATION",
+							Value:  "",
+							Children: []*services.TreeNode{
+								{
+									Symbol: "TYPE",
+									Value:  "",
+									Children: []*services.TreeNode{
+										{
+											Symbol: "KEYWORD",
+											Value:  "int",
+										},
+									},
+								},
+								{
+									Symbol: "IDENTIFIER",
+									Value:  "blue",
+								},
+								{
+									Symbol: "ASSIGNMENT",
+									Value:  "=",
+								},
+								{
+									Symbol: "EXPRESSION",
+									Value:  "",
+									Children: []*services.TreeNode{
+										{
+											Symbol: "TERM",
+											Value:  "",
+											Children: []*services.TreeNode{
+												{
+													Symbol: "INTEGER",
+													Value:  "13",
+												},
+											},
+										},
+										{
+											Symbol: "OPERATOR",
+											Value:  "+",
+										},
+										{
+											Symbol: "TERM",
+											Value:  "",
+											Children: []*services.TreeNode{
+												{
+													Symbol: "INTEGER",
+													Value:  "89",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						{
+							Symbol: "SEPARATOR",
+							Value:  ";",
+						},
+					},
+				},
+				{
+					Symbol: "FUNCTION",
+					Value:  "",
+					Children: []*services.TreeNode{
+						{
+							Symbol: "STATEMENT",
+							Value:  "{",
+							Children: []*services.TreeNode{
+								{
+									Symbol: "DECLARATION",
+									Value:  "",
+									Children: []*services.TreeNode{
+										{
+											Symbol: "TYPE",
+											Value:  "",
+											Children: []*services.TreeNode{
+												{
+													Symbol: "KEYWORD",
+													Value:  "int",
+												},
+											},
+										},
+										{
+											Symbol: "IDENTIFIER",
+											Value:  "blue",
+										},
+										{
+											Symbol: "ASSIGNMENT",
+											Value:  "=",
+										},
+										{
+											Symbol: "EXPRESSION",
+											Value:  "",
+											Children: []*services.TreeNode{
+												{
+													Symbol: "TERM",
+													Value:  "",
+													Children: []*services.TreeNode{
+														{
+															Symbol: "INTEGER",
+															Value:  "13",
+														},
+													},
+												},
+												{
+													Symbol: "OPERATOR",
+													Value:  "+",
+												},
+												{
+													Symbol: "TERM",
+													Value:  "",
+													Children: []*services.TreeNode{
+														{
+															Symbol: "INTEGER",
+															Value:  "89",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+								{
+									Symbol: "SEPARATOR",
+									Value:  "}",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	symbol_table, err := services.CreateSymbolTable(scope_rules, syntax_tree)
+
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	} else {
+
+		for _, scope := range symbol_table.SymbolScopes {
+			symbol, err := scope["blue"]
+			if err != true {
+				t.Errorf("Error: blue not found")
+			}
+			if symbol.Name != "blue" && symbol.Scope != 0 && symbol.Type != "int" {
+				t.Errorf("Symbol is incorrect: %v %v %v", symbol.Name, symbol.Scope, symbol.Type)
+			}
+		}
+
+	}
+
+}
