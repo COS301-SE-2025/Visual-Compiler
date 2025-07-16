7f81f0db0fbd8c4e61eb30473aed68863b4610af
DevanDewet <devandewet@gmail.com>
2025-07-16 11:42:02 +0200

    #258/feature/added skelton interface for analyser and translator

---
+++ visual-compiler/frontend/src/lib/components/analyser/analyser-artifact-viewer.svelte
@@ -0,0 +1,10 @@
+<div class="panel-container">
+    <h2>Symbol Table</h2>
+    <p>This panel will display the result of the analysis, such as an annotated syntax tree or a symbol table.</p>
+    </div>
+
+<style>
+    .panel-container {
+        padding: 1rem;
+    }
+</style>
