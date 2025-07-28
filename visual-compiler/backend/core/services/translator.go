8b4154916d2f976a16497e10653bd520b713c6ad
sashen-tv <sashengajai.sg@gmail.com>
2025-07-28 04:13:47 +0200

    Added translation rules reader

--- visual-compiler/backend/core/services/translator.go
+++ visual-compiler/backend/core/services/translator.go
@@ -12,3 +12,71 @@ type TranslationRule struct {
 	Sequence    []string
 	Translation []string
 }
+
+// Struct to track whether the syntax tree leaves are translated
+type TokenTracker struct {
+	Value string
+	Usage bool
+}
+
+// Name: ReadTranslationRules
+//
+// Parameters: []byte
+//
+// Return: Translator, error
+//
+// Receive an array of translation rules and return them in the struct
+func ReadTranslationRules(input []byte) ([]TranslationRule, error) {
+
+	translator := []TranslationRule{}
+
+	if len(input) == 0 {
+		return []TranslationRule{}, fmt.Errorf("no translation rules entered")
+	}
+
+	err := json.Unmarshal(input, &translator)
+
+	if err != nil {
+		return []TranslationRule{}, fmt.Errorf("invalid JSON for translation rules: %v", err)
+	}
+
+	delimiter := regexp.MustCompile(`[,\s]+`)
+
+	for i, rule := range translator {
+
+		sequence := []string{}
+
+		for _, entry := range rule.Sequence {
+			atoms := delimiter.Split(entry, -1)
+			sequence = append(sequence, atoms...)
+		}
+
+		translator[i].Sequence = sequence
+
+		token := regexp.MustCompile(`\{[^}]+\}`)
+
+		for _, entry := range rule.Translation {
+
+			placeholders := token.FindAllString(entry, -1)
+
+			for _, ph := range placeholders {
+
+				label := ph[1 : len(ph)-1]
+				found := false
+
+				for _, token := range sequence {
+					if token == label {
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					return []TranslationRule{}, fmt.Errorf("token {%s} in translation rule not found in sequence: %v", label, sequence)
+				}
+			}
+		}
+	}
+
+	return translator, nil
+}
