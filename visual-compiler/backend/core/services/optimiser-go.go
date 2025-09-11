9a22bc4df51eeae8db72d380e2a6f3e52f35b8e9
Tia-H <tiaharripersad@gmail.com>
2025-09-11 22:57:54 +0200

    feature/go optimiser stucture

--- visual-compiler/backend/core/services/optimiser-go.go
+++ visual-compiler/backend/core/services/optimiser-go.go
@@ -2,6 +2,13 @@ package services
 
 import "fmt"
 
+// Name: OptimiseGoCode
+//
+// Parameters: string, bool, bool, bool
+//
+// Return: string,error
+//
+// Receive Go lang code and perform optimisation based on the techniques selected
 func OptimiseGoCode(code string, constant_folding bool, dead_code bool, loop_unroling bool) (string, error) {
 
 	if code == "" {
@@ -9,26 +16,48 @@ func OptimiseGoCode(code string, constant_folding bool, dead_code bool, loop_unr
 	}
 
 	if constant_folding {
-		PerformConstantFolding(&code)
+		code = PerformConstantFolding(code)
 	}
 	if dead_code {
-		PerformDeadCodeElimination(&code)
+		code = PerformDeadCodeElimination(code)
 	}
 	if loop_unroling {
-		PerformLoopUnrolling(&code)
+		code = PerformLoopUnrolling(code)
 	}
 
 	return code, nil
 }
 
-func PerformConstantFolding(code *string) string {
-	return *code
+// Name: PerformConstantFolding
+//
+// Parameters: *string
+//
+// Return: string
+//
+// Perform constant folding on the source code
+func PerformConstantFolding(code string) string {
+	return code
 }
 
-func PerformDeadCodeElimination(code *string) string {
-	return *code
+// Name: PerformDeadCodeElimination
+//
+// Parameters: *string
+//
+// Return: string
+//
+// Perform dead code elimination on the source code
+func PerformDeadCodeElimination(code string) string {
+
+	return code
 }
 
-func PerformLoopUnrolling(code *string) string {
-	return *code
+// Name: PerformLoopUnrolling
+//
+// Parameters: *string
+//
+// Return: string
+//
+// Perform loop unrolling on the source code
+func PerformLoopUnrolling(code string) string {
+	return code
 }
