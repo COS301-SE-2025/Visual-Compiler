ba32d91e7c8d5d2c1b49816d3c3a7ce0148b06f1
Tia-H <tiaharripersad@gmail.com>
2025-09-12 12:16:57 +0200

    feature/optimiser: parse code and stringify AST completed

--- visual-compiler/backend/core/services/optimiser.go
+++ visual-compiler/backend/core/services/optimiser.go
@@ -1,8 +1,12 @@
 package services
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
 	"go/types"
 )
 
@@ -19,40 +23,82 @@ func OptimiseGoCode(code string, constant_folding bool, dead_code bool, loop_unr
 		return "", fmt.Errorf("code is empty")
 	}
 
+	ast, file_set, err := ParseGoCode(code)
+	if err != nil {
+		return "", err
+	}
+
 	if constant_folding {
-		code = PerformConstantFolding(code)
+		PerformConstantFolding(ast)
 	}
 	if dead_code {
-		code = PerformDeadCodeElimination(code)
+		PerformDeadCodeElimination(ast)
 	}
 	if loop_unroling {
-		code = PerformLoopUnrolling(code)
+		PerformLoopUnrolling(ast)
+	}
+
+	code, err = StringifyAST(ast, file_set)
+	if err != nil {
+		return "", err
 	}
 
 	return code, nil
 }
 
+// Name:ParseGoCode
+//
+// Parameters: string
+//
+// Return: *ast.File,error
+//
+// Receive Go lang code and create an Abstract Syntax Tree
+func ParseGoCode(code string) (*ast.File, *token.FileSet, error) {
+	file_set := token.NewFileSet()
+	ast, err := parser.ParseFile(file_set, "", code, parser.AllErrors)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return ast, file_set, nil
+}
+
+// Name: StringifyAST
+//
+// Parameters: *ast.File, *token.FileSet
+//
+// Return: string
+//
+// Converts optimised AST to a string
+func StringifyAST(ast *ast.File, file_set *token.FileSet) (string, error) {
+	var buffer bytes.Buffer
+	err := format.Node(&buffer, file_set, ast)
+	if err != nil {
+		return "", err
+	}
+	return buffer.String(), nil
+}
+
 // Name: PerformConstantFolding
 //
-// Parameters: *string
+// Parameters: *ast.File
 //
 // Return: string
 //
 // Perform constant folding on the source code
-func PerformConstantFolding(code string) string {
-	return code
+func PerformConstantFolding(ast *ast.File) {
+
 }
 
 // Name: PerformDeadCodeElimination
 //
-// Parameters: *string
+// Parameters: *ast.File
 //
 // Return: string
 //
 // Perform dead code elimination on the source code
-func PerformDeadCodeElimination(code string) string {
+func PerformDeadCodeElimination(ast *ast.File) {
 
-	return code
 }
 
 func EliminateDeadCode(fn *ast.FuncDecl, info *types.Info) {
@@ -61,11 +107,11 @@ func EliminateDeadCode(fn *ast.FuncDecl, info *types.Info) {
 
 // Name: PerformLoopUnrolling
 //
-// Parameters: *string
+// Parameters: *ast.File
 //
 // Return: string
 //
 // Perform loop unrolling on the source code
-func PerformLoopUnrolling(code string) string {
-	return code
+func PerformLoopUnrolling(ast *ast.File) {
+
 }
--- visual-compiler/backend/core/unit-tests/optimiser-unit_test.go
+++ visual-compiler/backend/core/unit-tests/optimiser-unit_test.go
@@ -1,10 +1,86 @@
 package unit_tests
 
 import (
+	"go/token"
 	"testing"
 
 	"github.com/COS301-SE-2025/Visual-Compiler/backend/core/services"
 )
+
+func TestParseCode_Success(t *testing.T) {
+	code := "package main\n"
+	code += "func NewFunction() int {\n"
+	code += "return 13\n"
+	code += "}"
+
+	ast, _, err := services.ParseGoCode(code)
+	if err != nil {
+		t.Errorf("Error :%v", err)
+	}
+	if ast == nil {
+		t.Errorf("AST is empty")
+	}
+}
+
+func TestParseCode_NoPackage(t *testing.T) {
+	code := "num := 2"
+
+	_, _, err := services.ParseGoCode(code)
+	if err == nil {
+		t.Errorf("Error expected:  expected 'package'")
+	}
+}
+
+func TestStringifyAST_Success(t *testing.T) {
+	code := "package main\n"
+	code += "func NewFunction() int {\n"
+	code += "return 13\n"
+	code += "}"
+
+	expected_result := "package main\n\n"
+	expected_result += "func NewFunction() int {\n"
+	expected_result += "\treturn 13\n"
+	expected_result += "}\n"
+
+	ast, file_set, err := services.ParseGoCode(code)
+	if err != nil {
+		t.Errorf("Error :%v", err)
+	}
+	if ast == nil {
+		t.Errorf("AST is empty")
+	}
+
+	code, err = services.StringifyAST(ast, file_set)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if code != expected_result {
+		t.Errorf("Stringify AST failed:\n %v\n %v", code, expected_result)
+	}
+}
+
+func TestStringifyAST_Fail(t *testing.T) {
+	code := "package main\n"
+	code += "func NewFunction() int {\n"
+	code += "return 13\n"
+	code += "}"
+
+	ast, _, err := services.ParseGoCode(code)
+	file_set := token.NewFileSet()
+	if err != nil {
+		t.Errorf("Error :%v", err)
+	}
+	if ast == nil {
+		t.Errorf("AST is empty")
+	}
+
+	_, err = services.StringifyAST(ast, file_set)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+}
 
 func TestOptimiseGo_NoInputCode(t *testing.T) {
 	code := ""
@@ -15,7 +91,7 @@ func TestOptimiseGo_NoInputCode(t *testing.T) {
 	}
 }
 
-func TestPerformDeadCodeElimination_Simple(t *testing.T) {
+/*func TestPerformDeadCodeElimination_Simple(t *testing.T) {
 	code := "package main\n"
 	code += "func NewFunction() int {\n"
 	code += "return 13\n"
@@ -35,4 +111,4 @@ func TestPerformDeadCodeElimination_Simple(t *testing.T) {
 	if optmised_code != expected_result {
 		t.Errorf("Optimisation failed : \n %v", optmised_code)
 	}
-}
+}*/
