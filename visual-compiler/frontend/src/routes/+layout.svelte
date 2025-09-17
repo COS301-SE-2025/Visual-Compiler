6e2be5dbbab77d96d1012f4c1bfc0b3883ca1242
DevanDewet <devandewet@gmail.com>
2025-09-17 10:27:16 +0200

    #497/feature/ai-chatbot

--- visual-compiler/frontend/src/routes/+layout.svelte
+++ visual-compiler/frontend/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import ToastContainer from '$lib/components/toast-conatiner.svelte';
+	import AiChatbot from '$lib/components/ai-chatbot/ai-chatbot.svelte';
 </script>
 
 <ToastContainer />
+<AiChatbot />
 <slot />
