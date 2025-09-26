0adecd64285ba3d365a0c46558218c3106bc804f
Tia-H <tiaharripersad@gmail.com>
2025-09-27 01:09:32 +0200

    testing/quality-tests/performance tests updated

--- visual-compiler/quality-tests/performance-tests/parser-test.js
+++ visual-compiler/quality-tests/performance-tests/parser-test.js
@@ -3,10 +3,14 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-      {duration: '20s', target: 10},
-      {duration: '20s', target: 10},
+      {duration: '20s', target: 50},
+      {duration: '20s', target: 50},
       {duration: '20s', target: 0},
     ],
+    thresholds: {
+      http_req_duration: ["p(95)<5000"],
+      http_req_failed: ["rate<0.01"],
+    },
 }
 
 export default function () {
--- visual-compiler/quality-tests/performance-tests/source-code-test.js
+++ visual-compiler/quality-tests/performance-tests/source-code-test.js
@@ -3,12 +3,12 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-      {duration: '10s', target: 5},
-      {duration: '20s', target: 5},
-      {duration: '10s', target: 0},
+      {duration: '20s', target: 50},
+      {duration: '20s', target: 50},
+      {duration: '20s', target: 0},
     ],
     thresholds: {
-      http_req_duration: ["p(95)<500"],
+      http_req_duration: ["p(95)<1000"],
       http_req_failed: ["rate<0.01"],
     },
 }
